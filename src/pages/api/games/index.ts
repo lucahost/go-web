0edf59c22c1734b2ec8a2f5ae5304f63388c0ac6
Luca Hostettler <hostettler.luca1896@gmail.com>
2021-05-22 14:23:13 +0200

    Init Game Start / Find API

--- src/pages/api/games/index.ts
+++ src/pages/api/games/index.ts
@@ -1,35 +1,36 @@
-import { Game, PrismaClient } from '.prisma/client'
+import { Game, Prisma, PrismaClient } from '.prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { HttpMethod } from '../../../lib/types'
 
-type GameResponseData = Game[] | never
+type GameResponseData = Game[] | Game | never
 
 type CreateGameDto = {
-    userId: string
+    userId: number
 }
 
 type NextApiRequestWithCreateGameDto = NextApiRequest & CreateGameDto
 
 const prisma = new PrismaClient()
 
-const apiMethod = (
-    req: NextApiRequest,
+const apiMethod = async (
+    req: NextApiRequestWithCreateGameDto,
     res: NextApiResponse<GameResponseData>
 ) => {
-    const { method } = req
+    const { method, body } = req
 
     switch (method) {
         case HttpMethod.GET:
-            prisma.game.findMany({
-                where: {
-                    gameState: 0,
-                },
-            })
-            res.status(200).json()
+            const games = await prisma.game.findMany()
+            res.status(200).json(games)
             break
         case HttpMethod.POST:
-            // TODO: Start new game and return game state
-            res.status(200).json({ name: 'Post /games' })
+            // TODO: create new game and return game state
+            const createGameDto: CreateGameDto = body
+            const gameData = {
+                authorId: createGameDto.userId,
+            }
+            const newGame = await prisma.game.create({ data: gameData })
+            res.status(200).json(newGame)
             break
         default:
             res.setHeader('Allow', [HttpMethod.GET, HttpMethod.POST])
