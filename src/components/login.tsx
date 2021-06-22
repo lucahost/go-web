8d7952b7c10185aabcb7ebc3fcae07a51dc11f59
Luca Hostettler <hostettler.luca1896@gmail.com>
2021-06-22 10:34:51 +0200

    Update Login Form

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,6 +3,28 @@ import useLocalStorage from '../lib/hooks/useLocalStorage'
 import { User } from '../lib/types'
 import axios from 'axios'
 import Spinner from './spinner'
+import styled from 'styled-components'
+
+const FieldInput = styled.input`
+    width: 25%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+`
+
+const LoginButton = styled.button`
+    width: 25%;
+    background: linear-gradient(20deg, #e66465, #9198e5);
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+`
 
 const Login: FC = () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -50,9 +72,17 @@ const Login: FC = () => {
         <>
             <h1>Bitte anmelden</h1>
             {error && <p>{error}</p>}
-            <input onChange={handleNameInput} placeholder="Name eingeben" />
-            <input onChange={handleEmailInput} placeholder="Email eingeben" />
-            <button onClick={handleLogin}>Go</button>
+            <FieldInput
+                name="name"
+                onChange={handleNameInput}
+                placeholder="Name eingeben"
+            />
+            <FieldInput
+                name="email"
+                onChange={handleEmailInput}
+                placeholder="Email eingeben"
+            />
+            <LoginButton onClick={handleLogin}>Go</LoginButton>
         </>
     )
 }
