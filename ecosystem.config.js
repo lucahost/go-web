1e263c94e45ac51ea5cd1a9edbf2943508e4315c
Luca Hostettler <luca@hostettler.io>
2025-12-17 21:36:12 +0100

    fix: dont bind webapp to default interface

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "FWebT GO",
         script: "node_modules/next/dist/bin/next",
-        args: "start",
+        args: "start -H 127.0.0.1",
         instances: 1,
         exec_mode: "fork"
     }]
